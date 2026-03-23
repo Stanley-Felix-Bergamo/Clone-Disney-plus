1a7823559b9cc22b1a458fc94d65a55629b6d44e
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2026-03-23 15:00:41 -0300

    Add shows

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@ const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
 const cleanCSS = require("gulp-clean-css");
+const terser = require("gulp-terser");
+function comprimeJs() {
+  return gulp
+    .src("./src/js/**/*.js")
+    .pipe(terser())
+    .pipe(gulp.dest("./dist/js"));
+}
+
 function comprimeImagens() {
   return gulp
     .src("./src/images/**/*")
@@ -11,19 +19,21 @@ function comprimeImagens() {
 
 function compileSass() {
   return gulp
-    .src("./src/styles/**/*.scss") 
-    .pipe(sass().on("error", sass.logError)) 
-    .pipe(cleanCSS())                       
+    .src("./src/styles/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(cleanCSS())
     .pipe(gulp.dest("./dist/css"));
 }
 
 exports.images = comprimeImagens;
 exports.sass = compileSass;
+exports.js = compileSass;
 
 exports.default = gulp.series(
-  gulp.parallel(compileSass, comprimeImagens),
+  gulp.parallel(compileSass, comprimeImagens, comprimeJs),
   function () {
     gulp.watch("./src/styles/**/*.scss", compileSass);
     gulp.watch("./src/images/**/*", comprimeImagens);
+    gulp.watch("./src/js/**/*", comprimeJs);
   },
 );
